d3b87990ec0e2c6fb61256d72acc26d5ae7450d1
E32201063-wahyu-rahmatullah <81340800+E32201063-wahyu-rahmatullah@users.noreply.github.com>
2021-04-22 14:33:23 +0700

    tugas minggu 7

---
+++ Minggu 7/routes/web.php
@@ -0,0 +1,81 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WahyuController;
+use App\Http\Controllers\Frontend\HomeController;
+use App\Http\Controllers\Backend\DashboardController;
+use App\Http\Controllers\Auth\LoginController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// ini route sederhana contoh 1
+route::get('/hello', function (){ 	
+	return 'hello World';
+});
+
+// ini  route sederhana contoh 2
+route::get('/belajar', function (){
+	echo'<h1>Hello World</h1>';
+	echo'<p> sedang belajar laravel</p>';
+});
+
+// ini route parameter 
+route::get('page/{nomor}', function($nomor){
+	return 'ini Halaman ke-' . $nomor;
+});
+
+//ini route untuk controller yang menggunakan beberapa method di dalam nya
+route::get('user',[WahyuController::class,'index']);
+route::resource('user',WahyuController::class);
+
+// ini route dalam membuat view sederhana
+route::get("/home", function()
+	{return view("home");
+});
+
+//ini adalah bab minggu ke 5
+route::group(['namespace' => 'Frontend'], function()
+	{
+		route::resource('home', 'HomeController');
+	});
+
+route::group(['namespace' => 'Backend'], function()
+	{
+		route::resource('dashboard', 'DashboardController');
+	});
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+//ini adalah route minggu ke 7
+Auth::routes();
+
+Route::group(['namespace' => 'Frontend'], function ()
+{
+	route::resource('home','HomeController');
+});
+
+
+route::group(['middleware' => ['web','auth']], function(){
+	route::resource('home','HomeController');
+	route::group(['namespace'=>'Backend'], function ()
+	{	
+	route::resource('dashboard', 'DashboardController');
+	});
+});
